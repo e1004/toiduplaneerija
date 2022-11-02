b407901745706178a43254850617f5f316648a47
Elisabeth Räni <34979322+e1004@users.noreply.github.com>
2022-11-02 20:52:56 +0200

    Lisa tühja toidukorra sisestaja (#8)

---
+++ test/conftest.py
@@ -0,0 +1,15 @@
+import sqlite3
+from contextlib import closing
+from test.db import TEST_DB_NAME
+
+import pytest
+
+from db import CREATE_TABLE_MEAL
+
+
+@pytest.fixture
+def db_connection():
+    with closing(sqlite3.connect(TEST_DB_NAME, uri=True)) as connection:
+        connection.execute(CREATE_TABLE_MEAL)
+        yield connection
+        connection.execute("DELETE FROM meal")
---
+++ test/db.py
@@ -0,0 +1,1 @@
+TEST_DB_NAME = "file:mem1?mode=memory&cache=shared"
