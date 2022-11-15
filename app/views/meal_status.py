997b78c984d4289b33e4242333d5348502a4db80
Elisabeth <34979322+e1004@users.noreply.github.com>
2022-11-15 20:58:32 +0200

    Lisa nupud

--- app/views/meal_status.py
+++ app/views/meal_status.py
@@ -1,5 +1,7 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QGridLayout, QLabel, QWidget
+from PyQt6.QtWidgets import QGridLayout, QLabel, QPushButton, QWidget
+
+from app import meal as meal_repo
 
 DAYS = [
     "esmaspäev",
@@ -21,6 +23,7 @@ class MealStatusView(QWidget):
         self.setLayout(self.grid)
         self._show_weekday_names()
         self._show_meal_type_names()
+        self._show_meal_buttons()
 
     def _show_weekday_names(self):
         for i, day in enumerate(DAYS, start=1):
@@ -31,3 +34,17 @@ class MealStatusView(QWidget):
             label = QLabel(meal_type.capitalize(), self)
             label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
             self.grid.addWidget(label, 0, i)
+
+    def _show_meal_buttons(self):
+        weekday_and_type_to_meal = meal_repo.read_all_meals()
+        for i in range(1, len(DAYS) + 1):
+            weekday = DAYS[i - 1]
+            for j in range(1, len(MEAL_TYPES) + 1):
+                meal_type = MEAL_TYPES[j - 1]
+                try:
+                    meal = weekday_and_type_to_meal[(weekday, meal_type)]
+                except KeyError:
+                    button = QPushButton("❌")
+                else:
+                    button = QPushButton("✅") if meal.name else QPushButton("❌")
+                self.grid.addWidget(button, i, j)
--- test/views/test_meal_status.py
+++ test/views/test_meal_status.py
@@ -1,6 +1,15 @@
-import pytest
+from test.db import TEST_DB_NAME
 
+import pytest
+from pytest_mock import MockerFixture
+
+from app import meal
 from app.views.meal_status import DAYS, MEAL_TYPES, MealStatusView
+
+
+@pytest.fixture
+def use_test_db(db_connection, mocker: MockerFixture):
+    mocker.patch.object(meal, "DB_NAME", TEST_DB_NAME)
 
 
 @pytest.fixture
@@ -42,3 +51,36 @@ def test_it_has_only_4_meal_types(view: MealStatusView):
 
     # then
     assert result is None
+
+
+@pytest.mark.usefixtures("use_test_db")
+def test_it_has_buttons_for_nameless_meals(view: MealStatusView):
+    # when
+    result = [
+        view.grid.itemAtPosition(i, j).widget().text()
+        for i in range(1, 8)
+        for j in range(1, 5)
+    ]
+
+    # then
+    assert result == ["❌" for i in range(28)]
+
+
+@pytest.mark.usefixtures("use_test_db")
+def test_it_has_buttons_for_meals_with_names(qtbot):
+    # given
+    weekday = "teisipäev"
+    meal_type = "hommik"
+    meal.add_empty_meal(weekday, meal_type)
+    meal.update_meal_name("any_name", weekday, meal_type)
+    view = MealStatusView()
+
+    # when
+    result = [
+        view.grid.itemAtPosition(i, j).widget().text()
+        for i in range(1, 8)
+        for j in range(1, 5)
+    ]
+
+    # then
+    assert result == ["❌" for i in range(4)] + ["✅"] + ["❌" for i in range(23)]
